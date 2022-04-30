b9d1587d581f9bdc7821b3a736306a5ef0447f24
jamg3c <31072623+jamg3c@users.noreply.github.com>
2022-04-30 21:13:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('My mon\'s news healthy dinner')
+streamlit.title('My Parents New healthy dinner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
