c9b9711f44978e9b0d1c43194318edfd11ce3f48
jamg3c <31072623+jamg3c@users.noreply.github.com>
2022-04-30 21:29:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-streamlit.tittle('Breakfast Favorites')
+streamlit.title('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
